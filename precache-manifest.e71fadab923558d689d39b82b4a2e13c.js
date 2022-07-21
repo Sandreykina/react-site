ab9bb457531d1cb5fb57227eb1f26f8dad61aee9
DESKTOP-FHT2P65\Elena <hellens.rez15@gmail.com>
2022-07-22 00:41:39 +0300

    Updates

---
+++ precache-manifest.e71fadab923558d689d39b82b4a2e13c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b851fdbe160014f09891",
+    "url": "/react-site/static/css/main.9501a1d8.chunk.css"
+  },
+  {
+    "revision": "b851fdbe160014f09891",
+    "url": "/react-site/static/js/main.b851fdbe.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/react-site/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "bbd99fed4c0c7d245502",
+    "url": "/react-site/static/js/2.bbd99fed.chunk.js"
+  },
+  {
+    "revision": "2f01a33d54fec8105ddd",
+    "url": "/react-site/static/js/runtime~main.2f01a33d.js"
+  },
+  {
+    "revision": "00a547964dfeb2bd98d96bf4c584a14d",
+    "url": "/react-site/static/media/logo.00a54796.svg"
+  },
+  {
+    "revision": "9c04290518e1425265a38d74ceaf1d79",
+    "url": "/react-site/index.html"
+  }
+];
